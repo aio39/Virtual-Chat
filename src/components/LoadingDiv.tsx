6dbd48d475130b1e590da7b96d00214b8154a0f3
Aio <AioDev.JS@gmail.com>
2021-11-28 04:23:21 +0900

    상대방 로딩 애니메이션

--- src/components/LoadingDiv.tsx
+++ src/components/LoadingDiv.tsx
@@ -1,59 +1,24 @@
-import { Box } from '@chakra-ui/layout';
+import { Center } from '@chakra-ui/layout';
 import { motion } from 'framer-motion';
-
-const loadingContainerVariants = {
-  start: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-  end: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const loadingCircleVariants = {
-  start: {
-    y: '0%',
-  },
-  end: {
-    y: '100%',
-  },
-};
-
-const loadingCircleTransition = {
-  duration: 0.5,
-  yoyo: Infinity,
-  ease: 'easeInOut',
-};
 
 const LoadingDiv = () => {
   return (
-    <motion.div
-      style={{ width: 100, height: 100 }}
-      variants={loadingContainerVariants}
-      initial="start"
-      animate="end"
-    >
-      <Box flexGrow=""></Box>
-      <motion.span
-        style={{ width: 20, height: 20, backgroundColor: 'red' }}
-        variants={loadingCircleVariants}
-        transition={loadingCircleTransition}
-      />
-      <motion.span
-        style={{ width: 20, height: 20, backgroundColor: 'red' }}
-        variants={loadingCircleVariants}
-        transition={loadingCircleTransition}
-      />
-      <motion.span
-        style={{ width: 20, height: 20, backgroundColor: 'red' }}
-        variants={loadingCircleVariants}
-        transition={loadingCircleTransition}
-      />
-    </motion.div>
+    <Center backgroundColor="black" width="full" height="full">
+      <motion.div
+        className="lds-roller"
+        initial={{ scale: 0 }}
+        animate={{ scale: 3.0 }}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </motion.div>
+    </Center>
   );
 };
 
